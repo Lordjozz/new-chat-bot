28db0b3800a6627dce26ff2747fb8f25628d0072
Paul <paulrobertrayner@hotmail.co.uk>
2022-09-14 10:41:49 +0100

    basic chat box component style

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,65 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import { view } from '@risingstack/react-easy-state'
-import {Link} from 'react-router-dom'
-import layout from './layout.module.scss'
+import React, { useEffect, useState } from 'react';
+import { view } from '@risingstack/react-easy-state';
+import { Link } from 'react-router-dom';
+import layout from './layout.module.scss';
 // import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader'
 // import GetCountryFromISO from '../../helpers/get-country-from-iso'
-import GetUserGeo from '../../helpers/get-user-geo'
-import GameStore from '../../stores/game'
-import UserStore from '../../stores/user'
-import GoogleFontLoader from 'react-google-font-loader'
-import axios from 'axios'
+import GetUserGeo from '../../helpers/get-user-geo';
+import GameStore from '../../stores/game';
+import UserStore from '../../stores/user';
+import GoogleFontLoader from 'react-google-font-loader';
+import axios from 'axios';
 
 export default view(function Layout(props) {
-  const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false);
   useEffect(() => {
     // Get the game
 
     // Get user location
     // console.log('%cCountry', GetCountryFromISO('gb'))
-    GetUserGeo().then(geo => {
-      // console.log('GEO', geo)
-      UserStore.geo = geo
-      // console.log(GetUserCountry(geo))
-    }).catch(err => {
-      console.log('err', err)
-    })
+    GetUserGeo()
+      .then((geo) => {
+        // console.log('GEO', geo)
+        UserStore.geo = geo;
+        // console.log(GetUserCountry(geo))
+      })
+      .catch((err) => {
+        console.log('err', err);
+      });
 
-    axios.get(process.env.REACT_APP_GAME_API_ENDPOINT).then(e => {
-      if (e.status === 200) {
-        GameStore.game = e.data
-        setLoaded(true)
-      }
-    }).catch(e => {
-      window.alert('Issue loading game', e.response ? JSON.stringify(e.response.data) : null)
-    })
+    // axios.get(process.env.REACT_APP_GAME_API_ENDPOINT).then(e => {
+    //   if (e.status === 200) {
+    //     GameStore.game = e.data
+    //     setLoaded(true)
+    //   }
+    // }).catch(e => {
+    //   window.alert('Issue loading game', e.response ? JSON.stringify(e.response.data) : null)
+    // })
 
-    document.documentElement.style.setProperty('--fonts', GameStore.game ? GameStore.game.Styling.FontFamily : 'Roboto')
-  }, [])
-  return (
-    loaded ? <>
-      <div className={layout.layout}>
-        <GoogleFontLoader fonts={[{
-          font: GameStore.game ? GameStore.game.Styling.FontFamily : 'Roboto',
-          weights: [400, '400i'],
-        }]} />
-        <div className={layout.nav}>
-          <img alt="" className={layout.logo} src={GameStore.game ? GameStore.game.Styling.Logo : null} />
-          {
-            UserStore.setup ? <Link to="/rooms">Chat Rooms</Link> : <Link to="/profile">Your Profile</Link>
-          }
-          
-          {/* {!!UserStore.name ?  : null} */}
-        </div>
+    document.documentElement.style.setProperty(
+      '--fonts',
+      GameStore.game ? GameStore.game.Styling.FontFamily : 'Roboto'
+    );
+  }, []);
+  return !loaded ? (
+    <>
+      <Layout className={layout.layout}>
         <div>
-          {props.children}
+          <GoogleFontLoader
+            fonts={[
+              {
+                font: GameStore.game
+                  ? GameStore.game.Styling.FontFamily
+                  : 'Roboto',
+                weights: [400, '400i'],
+              },
+            ]}
+          />
+          {/* <div className={layout.nav}> */}
+          {/* <img
+            alt=""
+            className={layout.logo}
+            src={GameStore.game ? GameStore.game.Styling.Logo : null}
+          /> */}
+
+          {/* {!!UserStore.name ?  : null} */}
+          {/* </div> */}
+          <div>{props.children}</div>
         </div>
-      </div>
-    </> : <>
+      </Layout>
+    </>
+  ) : (
+    <>
       <div>
         <h1>Loading</h1>
         {/* <ClimbingBoxLoader loading={true} size={15} color="var(--dark-background)" /> */}
       </div>
     </>
-  )
-})
+  );
+});
--- src/pages/room.jsx
+++ src/pages/room.jsx
@@ -1,86 +1,143 @@
-import { view } from '@risingstack/react-easy-state'
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useRouteMatch } from 'react-router'
-import Layout from '../components/layout/layout'
-import GameStore from '../stores/game'
-import UserStore from '../stores/user'
+import { view } from '@risingstack/react-easy-state';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useRouteMatch } from 'react-router';
+import Layout from '../components/layout/layout';
+import GameStore from '../stores/game';
+import UserStore from '../stores/user';
+import SendIcon from '@mui/icons-material/Send';
 
 export default view(function RoomPage() {
-  const match = useRouteMatch()
+  const match = useRouteMatch();
 
-  const chatRoom = GameStore.game.ChatRooms.find(_ => _.Name === decodeURI(match.params.name))
-  const messages = GameStore.messageHistory.filter(_ => _.Room === chatRoom.Name)
+  // const chatRoom = GameStore.game.ChatRooms.find(
+  //   (_) => _.Name === decodeURI(match.params.name)
+  // );
+  // const messages = GameStore.messageHistory.filter(
+  //   (_) => _.Room === chatRoom.Name
+  // );
 
-  useEffect(() => {
-    if (messages.length === 0) {
-      chatRoom.InitialMessages.forEach(msg => {
-        GameStore.sendMessage(msg.Message, msg.From, true, chatRoom.Name, msg.Colour)
-      })
-    }
-  }, []) // eslint-disable-line
+  const chatRoom = { Name: 'Klaus' };
+
+  const messages = [
+    { id: 23, From: 'you', Message: 'Hey there' },
+    { id: 23, From: '', Message: 'Im klaus' },
+  ];
+
+  // useEffect(() => {
+  //   if (messages.length === 0) {
+  //     chatRoom.InitialMessages.forEach((msg) => {
+  //       GameStore.sendMessage(
+  //         msg.Message,
+  //         msg.From,
+  //         true,
+  //         chatRoom.Name,
+  //         msg.Colour
+  //       );
+  //     });
+  //   }
+  // }, []); // eslint-disable-line
 
   const getTranscript = () => {
-    axios.post(process.env.REACT_APP_GAME_API_ENDPOINT + 'transcript', {messages, email: UserStore.email}).then(e => {
-      window.alert("We've sent you the chat history!")
-    }).catch(err => {
-      window.alert('We weren\'t able to send you the email.')
-    })
-  }
+    // axios.post(process.env.REACT_APP_GAME_API_ENDPOINT + 'transcript', {messages, email: UserStore.email}).then(e => {
+    //   window.alert("We've sent you the chat history!")
+    // }).catch(err => {
+    //   window.alert('We weren\'t able to send you the email.')
+    // })
+  };
 
-  const sendMessage = () => GameStore.sendMessage(GameStore.messageContent, null, false, chatRoom.Name)
+  const sendMessage = () => console.log('message');
+  // GameStore.sendMessage(GameStore.messageContent, null, false, chatRoom.Name);
+
+  function renderMessages() {
+    return messages.map((msg) => {
+      // const type = msg.From === UserStore.name ? 'you' : 'them';
+      const type = msg.From;
+      return type === 'you' ? (
+        <div className={type}>
+          <p>{msg.Message}</p>
+        </div>
+      ) : (
+        <div className={type}>
+          <div dangerouslySetInnerHTML={{ __html: msg.Message }} />
+        </div>
+      );
+    });
+  }
 
   return (
     <Layout>
       <div className="room">
-        <div>
-          <h1>{chatRoom.Name}</h1>
+        <div className="header">{chatRoom.Name}</div>
+        {/* <div>
           <button onClick={() => getTranscript()}>Get Transcription</button>
-        </div>
+        </div> */}
         <div className="chat">
-          <div className="messages" style={{gridArea: 'a'}}>
-            <div id="msgBox" style={{maxHeight: 500, overflowY: 'auto', display: 'flex', flexDirection: 'column'}}>
-              {/* Chat History */}
-              {
-                messages.map(msg => {
-                  const type = msg.From === UserStore.name ? 'you' : 'them'
-                  return (
-                    type === 'you' ? <div className={type}>
-                      <strong style={{color: msg.Colour}}>{msg.From}</strong>
-                      <p>{msg.Message}</p>
-                    </div> : <div className={type}>
-                      <strong style={{color: msg.Colour}}>{msg.From}</strong>
-                      <div dangerouslySetInnerHTML={{__html: msg.Message}} />
-                    </div>
-                  )
-                })
-              }
+          <div className="messages" style={{ gridArea: 'a' }}>
+            <div
+              id="msgBox"
+              style={{
+                maxHeight: 500,
+                overflowY: 'auto',
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              {renderMessages()}
             </div>
           </div>
-          <div className="message-input" style={{gridArea: 'b'}}>
-            {/* Input */}
-            <input type="text" placeholder="Your message" onKeyPress={e => {console.log(e.key);if (e.key === 'Enter') {sendMessage()}}} onChange={_ => GameStore.messageContent = _.target.value} value={GameStore.messageContent} />
-            <button onClick={() => sendMessage()}>Send</button>
-          </div>
-          <div className="users" style={{gridArea: 'c'}}>
-            {/* Chat Members */}
-            <h2>Users</h2>
-            {
-              chatRoom.Users.filter(user => !GameStore.left.filter(gs => gs.Room === chatRoom.Name).find(_ => _.Name = user.Name)).map(user => {
-                return (<div>
-                  <p><strong>Name:</strong> {user.Name}</p>
-                  <p><strong>IP:</strong> {user.IP}</p>
-                </div>)
-              })
-            }
-            <div style={{backgroundColor: '#47ff51'}}>
-              <p><strong>Your Name:</strong> {UserStore.name}</p>
-              <p><strong>Your IP:</strong> 172.10.42.230</p>
+          <div className="messageWrap">
+            {' '}
+            <div className="message-input" style={{ gridArea: 'b' }}>
+              <input
+                type="text"
+                placeholder="Type message"
+                onKeyPress={(e) => {
+                  console.log(e.key);
+                  if (e.key === 'Enter') {
+                    sendMessage();
+                  }
+                }}
+                onChange={(_) => (GameStore.messageContent = _.target.value)}
+                value={GameStore.messageContent}
+              />
+              <div className="sendIconWrap">
+                <SendIcon onClick={() => sendMessage()} />
+              </div>
             </div>
           </div>
+
+          {/* <div className="users" style={{ gridArea: 'c' }}> */}
+          {/* Chat Members */}
+          {/* <h2>Users</h2>
+            {chatRoom.Users.filter(
+              (user) =>
+                !GameStore.left
+                  .filter((gs) => gs.Room === chatRoom.Name)
+                  .find((_) => (_.Name = user.Name))
+            ).map((user) => {
+              return (
+                <div>
+                  <p>
+                    <strong>Name:</strong> {user.Name}
+                  </p>
+                  <p>
+                    <strong>IP:</strong> {user.IP}
+                  </p>
+                </div>
+              );
+            })} */}
+          {/* <div style={{ backgroundColor: '#47ff51' }}>
+              <p>
+                <strong>Your Name:</strong> {UserStore.name}
+              </p>
+              <p>
+                <strong>Your IP:</strong> 172.10.42.230
+              </p>
+            </div> */}
+          {/* </div> */}
         </div>
-        
       </div>
     </Layout>
-  )
-})
+  );
+});
