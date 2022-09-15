f76ab2a3ea1d87e7fabac5f4a91bf82c86f68833
Paul <paulrobertrayner@hotmail.co.uk>
2022-09-15 11:39:29 +0100

    update font

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { view } from '@risingstack/react-easy-state';
 import { Link } from 'react-router-dom';
 import layout from './layout.module.scss';
-// import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader'
-// import GetCountryFromISO from '../../helpers/get-country-from-iso'
 import GetUserGeo from '../../helpers/get-user-geo';
 import GameStore from '../../stores/game';
 import UserStore from '../../stores/user';
@@ -14,14 +12,10 @@ export default view(function Layout(props) {
   const [loaded, setLoaded] = useState(false);
   useEffect(() => {
     // Get the game
-
     // Get user location
-    // console.log('%cCountry', GetCountryFromISO('gb'))
     GetUserGeo()
       .then((geo) => {
-        // console.log('GEO', geo)
         UserStore.geo = geo;
-        // console.log(GetUserCountry(geo))
       })
       .catch((err) => {
         console.log('err', err);
@@ -35,11 +29,6 @@ export default view(function Layout(props) {
     // }).catch(e => {
     //   window.alert('Issue loading game', e.response ? JSON.stringify(e.response.data) : null)
     // })
-
-    document.documentElement.style.setProperty(
-      '--fonts',
-      GameStore.game ? GameStore.game.Styling.FontFamily : 'Roboto'
-    );
   }, []);
   return !loaded ? (
     <>
@@ -49,28 +38,18 @@ export default view(function Layout(props) {
             {
               font: GameStore.game
                 ? GameStore.game.Styling.FontFamily
-                : 'Roboto',
+                : 'Overpass',
               weights: [400, '400i'],
             },
           ]}
         />
-        {/* <div className={layout.nav}> */}
-        {/* <img
-            alt=""
-            className={layout.logo}
-            src={GameStore.game ? GameStore.game.Styling.Logo : null}
-          /> */}
-
-        {/* {!!UserStore.name ?  : null} */}
-        {/* </div> */}
         <div>{props.children}</div>
       </div>
     </>
   ) : (
     <>
       <div>
-        <h1>Loading</h1>
-        {/* <ClimbingBoxLoader loading={true} size={15} color="var(--dark-background)" /> */}
+        <h1>Loading...</h1>
       </div>
     </>
   );
