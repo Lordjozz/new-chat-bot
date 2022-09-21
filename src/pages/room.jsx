8bdd4c6325ea4783a25633a719766dd1597b311d
Paul <paulrobertrayner@hotmail.co.uk>
2022-09-21 18:30:06 +0100

    test overlay scroll

--- src/pages/room.jsx
+++ src/pages/room.jsx
@@ -165,7 +165,7 @@ export default view(function RoomPage() {
               ref={onRefChange}
               style={{
                 maxHeight: `${messageWindow}px`,
-                overflowY: 'auto',
+                overflowY: 'overlay',
                 display: 'flex',
                 flexDirection: 'column',
                 marginBottom: '65px',
