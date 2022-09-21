476e35cbb4d7b7de40bec49dea5753e460558bd3
Paul <paulrobertrayner@hotmail.co.uk>
2022-09-21 16:53:15 +0100

    revert remove mobile scroll

--- src/pages/room.jsx
+++ src/pages/room.jsx
@@ -19,7 +19,7 @@ export default view(function RoomPage() {
   const storedHeight = localStorage.getItem('height');
   const desktop = window.screen.width > 900;
   const headerHeight = 63.5;
-  const messageBarHeight = desktop ? 120 : 165;
+  const messageBarHeight = desktop ? 120 : 100;
   const messageWindow = height - (headerHeight + messageBarHeight);
   const disableSubmit = !GameStore.messageContent;
   const storedCount = localStorage.getItem('messageCount');
@@ -168,7 +168,7 @@ export default view(function RoomPage() {
                 overflowY: 'scroll',
                 display: 'flex',
                 flexDirection: 'column',
-                marginBottom: isMobile ? '130px' : '65px',
+                marginBottom: '65px',
               }}
             >
               {renderMessages()}
